fa7c049c51b2065daeaf29e7c99f7088bd7f23d1
Destiny-16 <destiny6@mail.usf.edu>
2020-10-17 22:07:23 -0400

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 root = Tk()
 root.title("Hex Game")
@@ -14,10 +15,9 @@ computer = ""
 start = ""
 end = ""
 turn = ""
-p = ['0']*100
-c = ['0']*100
+edges = ['0']*50
 i = 0
-j = 0
+
 
 class Node:
     x1 = 0 
@@ -84,16 +84,16 @@ def setPlayers():
    choice = input("Whice Player? 1(solid) or 2(dashed)?: ")
 
    if(choice == '1'):
-        print("you are solid lines")
-        computer = "p2"
-        player = "p1"
-        turn = player
-        
-   elif(choice == '2'):
         print("you are dashed lines")
         computer = "p1"
         player = "p2"
         turn = computer
+        
+   elif(choice == '2'):
+        print("you are solid lines")
+        computer = "p2"
+        player = "p1"
+        turn = player
         
    else:
         print("invalid input")
@@ -104,9 +104,14 @@ def setPlayers():
         
 
     
-def drawLine(turn, player, computer, i, j):
-    start = input("Starting node: ")
-    end = input("Ending node: ")
+def drawLine(turn, player, computer, i):
+
+    if(turn == player):
+        start = input("Starting node: ")
+        end = input("Ending node: ")
+    else:
+        increment = ComputerChoice(i)
+        return increment
     
     x3 = 0
     y3 = 0
@@ -172,20 +177,17 @@ def drawLine(turn, player, computer, i, j):
         print("invalid entry")
         exit()
 
-    if(turn == "p1"):
+    increment = [node1, node2]
+    #checkLine(increment, edges)
+
+    if(turn == player and player == "p1"):
         draw.create_line(x3,y3,x4,y4,width=3)
-    else:
+    elif(turn == player and player == "p2"):
         draw.create_line(x3,y3,x4,y4,width=3, dash=(3,5))
-
-    if(turn == player):
-        p[i] = [node1, node2]
-        i = i + 1
-    elif( turn == computer):
-        c[j] = [node1, node2]
-        j = j + 1
+    
 
 
-    return [i,j]
+    return increment
 
         
 
@@ -203,18 +205,106 @@ def getPlayer(turn, player, computer):
 
         
 
-    
-def checkWinner():
-    return False;
 
-#def ComputerChoice():
+
+def ComputerChoice(i):
+    n_a = int(random.choice([n1.node, n2.node, n3.node, n4.node, n5.node, n6.node]))
+    n_b = int(random.choice([n1.node, n2.node, n3.node, n4.node, n5.node, n6.node]))
+
+    if(n_a == n_b):
+        ComputerChoice(i)
+    elif( (n_a - n_b == 1) or (n_b - n_a == 1)):
+        ComputerChoice(i)
+
+    n_a = str(n_a)
+    n_b = str(n_b)
+
+    if(n_a == n1.node):
+        x3 = n1.x1
+        y3 = n1.y1
+        node1 = n1.node
+    elif(n_a == n2.node):
+        x3 = n2.x1
+        y3 = n2.y1
+        node1 = n2.node
+    elif(n_a == n3.node):
+        x3 = n3.x1
+        y3 = n3.y1
+        node1 = n3.node
+    elif(n_a == n4.node):
+        x3 = n4.x1
+        y3 = n4.y1
+        node1 = n4.node
+    elif(n_a == n5.node):
+        x3 = n5.x1
+        y3 = n5.y1
+        node1 = n5.node
+    elif(n_a == n6.node):
+        x3 = n6.x1
+        y3 = n6.y1
+        node1 = n6.node
+  
+
+    if(n_b == n1.node):
+        x4 = n1.x2
+        y4 = n1.y2
+        node2 = n1.node
+    elif(n_b == n2.node):
+        x4 = n2.x2
+        y4 = n2.y2
+        node2 = n2.node
+    elif(n_b == n3.node):
+        x4 = n3.x2
+        y4 = n3.y2
+        node2 = n3.node
+    elif(n_b == n4.node):
+        x4 = n4.x2
+        y4 = n4.y2
+        node2 = n4.node
+    elif(n_b == n5.node):
+        x4 = n5.x2
+        y4 = n5.y2
+        node2 = n5.node
+    elif(n_b == n6.node):
+        x4 = n6.x2
+        y4 = n6.y2
+        node2 = n6.node
+        
+    increment = [node1, node2]
+    #checkLine(increment, edges)
+        
+
+    if(turn == computer and computer == "p1"):
+        draw.create_line(x3,y3,x4,y4,width=3)
+    elif(turn == computer and computer == "p2"):
+        draw.create_line(x3,y3,x4,y4,width=3, dash=(3,5))
     
+    increment = [node1, node2]
+
+    return increment
+
+
+def checkLine(increment, edges):
     
-    
+    for d in edges:
+        while(d == increment):
+            print("Line already drawn. Try again")
+            if(turn == player):
+                start = input("Starting node: ")
+                end = input("Ending node: ")
+            else:
+                increment = ComputerChoice(i)
+                return increment
+            
+        
+    drawLine(turn, player, computer, i)
+
+
+
 
 
 n = [0]*3
-increment = [0]*2
+increment = [0, 0]
 winner = False
 n = setPlayers()
 
@@ -226,15 +316,13 @@ computer = n[2]
 while(winner == False):
     print(turn, "'s", " turn")
     drawBoard()
-    increment = drawLine(turn, player, computer, i, j)
-    i = increment[0]
-    j = increment[1]
+    edges[i] = drawLine(turn, player, computer, i)
+    i = i + 1
     frame.update()
     turn = getPlayer(turn, player, computer)
-    winner = checkWinner()
+    #winner = checkWinner()
+    print(edges)
 
-    print('p', p)
-    print('c', c)
 
     
 
